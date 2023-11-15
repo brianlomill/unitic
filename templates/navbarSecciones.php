532825592ef0a967c04f7f496041e7aa426017a7
Brian <brianlo_mil.com>
2023-11-15 13:41:46 -0500

    se corrigio navbarSecciones con rutas y menus del navbar

--- templates/navbarSecciones.php
+++ templates/navbarSecciones.php
@@ -2,52 +2,77 @@
 <html lang="en">
 
 <head>
-  <!-- ESTILOS -->
-  <link rel="stylesheet" type="text/css" href="../css/navbar.css">
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="../css/navbar.css" rel="stylesheet">
 
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg bg-dark" id="navbar">
-    <div class="container">
-        <a class="navbar-brand" href="../public/index.php">
-            <img src="../img/logo2.WEBP" alt="" width="50" height="50" class="d-inline-block align-text-top">
-            <img src="../img/uniticestandar2.WEBP" alt="" width="150" height="50" class="d-inline-block align-text-top">
-        </a>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-dark" id="navbar">
+            <div class="container">
+                <a class="navbar-brand" href="../public/index.php">
+                    <img src="../img/logo2.WEBP" alt="" width="50" height="50" class="d-inline-block align-text-top">
+                    <img src="../img/uniticestandar2.WEBP" alt="" width="150" height="50" class="d-inline-block align-text-top">
+                </a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-        <div class="collapse navbar-collapse" id="navbarScroll">
-            <ul class="navbar-nav m-auto" style="--bs-scroll-height: 100px;">
-                <li class="nav-item">
-                    <a class="nav-link active text-warning" aria-current="page" href="../index.php">Inicio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active text-white" aria-current="page" href="../public/proyectos.php">Proyectos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active text-white" aria-current="page" href="../public/monografias.php">Monografias</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active text-white" aria-current="page" href="../public/integrantes.php">Integrantes</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active text-white" aria-current="page" href="#">Galeria</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active text-white" aria-current="page" href="#">Contactenos</a>
-                </li>
-            </ul>
-            <a name="" id="" class="btn btn-outline-warning" href="../admin/login.php" role="button">Administrador</a>
-        </div>
-    </div>
-</nav>
-    <!-- End of Page Navbar --> 
-  <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+                <div class="collapse navbar-collapse" id="navbarScroll">
+                    <ul class="navbar-nav m-auto" style="--bs-scroll-height: 100px;">
+                        <li class="nav-item">
+                            <a class="nav-link active text-warning" aria-current="page" href="../index.php">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active text-white" aria-current="page" href="../public/proyectos.php">Proyectos</a>
+                        </li>
+                        <li class="nav-item">
+                            <div class="dropdown">
+                                <a class="nav-link active text-white dropdown-toggle" href="#" role="button" id="productosDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Productos
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="productosDropdown">
+                                    <li><a class="dropdown-item" href="../public/monografias.php">Monografias</a></li>
+                                    <li><a class="dropdown-item" href="../public/ponencias.php">Ponencias</a></li>
+                                    <li><a class="dropdown-item" href="../public/posters.php">Posters</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active text-white" aria-current="page" href="../public/integrantes.php">Integrantes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active text-white" aria-current="page" href="#">Galeria</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active text-white" aria-current="page" href="#">Contactenos</a>
+                        </li>
+                    </ul>
+                    <a name="" id="" class="btn btn-outline-warning" href="../admin/login.php" role="button">Administrador</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
