ac38c76aa8ba385cc362a6c2848d2355940d1069
Brian <brianlo_mil.com>
2024-03-11 20:01:27 -0500

    Se fusiono stylesNuevo.css que venia mejorando a styles.css

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
   <!-- BOOTSTRAP ICONS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
   <!-- HOJA DE ESTILOS -->
-  <link rel="stylesheet" type="text/css" href="css/stylesNuevo.css">
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
   <!-- FONTAWESOME -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
 
